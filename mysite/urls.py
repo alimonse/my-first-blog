fa9c0b2cc72372de3eaa6103f0507c9341626297
Alisson <alimonse29@gmail.com>
2017-09-02 15:25:04 -0500

    Cambie el HTML para la pagina

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = [#patrones de los URLs para llamar a esas pags web
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
